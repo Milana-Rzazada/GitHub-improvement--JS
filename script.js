a78afb7f61061f09ab032fee75dce95f7b653115
Milana-Rzazade <71l1c0h@code.edu.az>
2025-07-26 22:47:01 -0400

    Mouse events are ready and working!

---
+++ script.js
@@ -0,0 +1,20 @@
+// Mouse Events
+const mouseBtn1 = document.getElementById('mouseBtn1');
+const mouseBtn2 = document.getElementById('mouseBtn2');
+const message = document.getElementById('message');
+const mouseMessage = document.getElementById('mouseMessage');
+
+mouseBtn1.addEventListener('mouseover', () => {
+  mouseMessage.textContent = 'You can send your message!';
+});
+
+mouseBtn2.addEventListener('mouseover', () => {
+  mouseMessage.textContent = 'You can cancel!';
+});
+
+message.addEventListener('mouseover', () =>{
+    mouseMessage.textContent = 'You have to write your message here!'
+});
+
+
+
